f45c89af329fe7101c8dcff20816e6fc0f880540
nikolayg <nikolay.grozev@gmail.com>
2018-01-19 20:25:17 +1100

    Basic Authentication.

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const path = require('path');
 const helmet = require('helmet');
 const request = require('request');
 const urljoin = require('url-join');
-const _ = require('lodash');
+const basicAuth = require('express-basic-auth');
 
 // Load the dotenv config. Get all extra headers from .env
 require('dotenv').config();
 const server = process.env.API_ENDPOINT;
 const headers = {};
 for (let key in process.env) {
-  if (key.startsWith('API_HEADER_')) {
-    const headerName = key.replace('API_HEADER_');
-    headers[headerName] = process.env[key];
-  }
+    if (key.startsWith('API_HEADER_')) {
+        const headerName = key.replace('API_HEADER_');
+        headers[headerName] = process.env[key];
+    }
 }
 
 // The app server
@@ -24,28 +24,37 @@ app.use(helmet());
 
 // Proxy the API
 app.use('/api', (req, res) => {
-  var url = urljoin(server, req.url);
+    const url = urljoin(server, req.url);
 
-  // Options to add API Connect headers
-  var requestOptions = { url, headers };
+    // Options to add API Connect headers
+    const requestOptions = { url, headers };
 
-  req.pipe(request(requestOptions)).pipe(res);
+    req.pipe(request(requestOptions)).pipe(res);
 });
+
+// Set up basic auth
+const user = process.env.BASIC_AUTH_USER || 'admin';
+const pass = process.env.BASIC_AUTH_PASS || 'admin';
+app.use(basicAuth({
+    users: { [user]: `${pass}` },    
+    challenge: true,
+    realm: 'Imb4T3st4pp'
+}));
 
 // Serve the static content
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Direct all requests to the main page so they can be handled by React Router
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Let's start ...
 const port = process.env.PORT || 3001;
 app.listen(port, err => {
-  if (err) {
-    console.log(`App crashed: ${err}`);
-  } else {
-    console.log(`Listening on port ${port}`);
-  }
+    if (err) {
+        console.log(`App crashed: ${err}`);
+    } else {
+        console.log(`Listening on port ${port}`);
+    }
 });
